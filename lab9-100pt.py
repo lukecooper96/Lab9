cc94bb2ab970f4ee9b4744875938275bc7abba51
Luke Cooper <luke.cooper96@gmail.com>
2014-10-23 13:12:38 -0500

    lab 9 100 percent

--- lab9-100pt.py
+++ lab9-100pt.py
@@ -13,3 +13,28 @@
 # - A temperature of over 105F
 # - A temperature of over 102F and they have been sick in the last 24 hours
 # - A temperature over 100, OR they've been sick in the last 24 hours, AND they've recently travelled to West Africa.
+
+diagnosis = 1
+yes = 'yes'
+no = 'no'
+while(diagnosis == 1):
+    print 'what is your temp?'
+    temp = int(raw_input())
+    print 'have you been sick in the last 24 house?'
+    sickLast = raw_input()
+    print 'have you recently traveled to west africa?'
+    travel = raw_input()
+    if temp < 100:
+        diagnosis = 0
+    if temp < 106:
+        if temp > 99:
+            if sickLast == no:
+                diagnosis = 0
+    if travel == yes:
+        if temp > 99:
+            diagnosis = 1
+        if sickLast == yes:
+            diagnosis = 1    
+    if diagnosis == 1:
+        print 'You have Ebola!'
+print 'You\'re Ebola free!'
